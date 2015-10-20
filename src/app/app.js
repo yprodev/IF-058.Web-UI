b82cc7abd93568c09654419c9be29509af2b3227
Vitaliy <bazyukvitaliy@gmail.com>
2015-10-20 12:22:50 +0300

    Added app.js

---
+++ src/app/app.js
@@ -0,0 +1,26 @@
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider) {
+	$routeProvider.
+	when('/', {
+		templateUrl: 'app/views/main.html',
+		controller: ''
+	}).
+	when('/groups', {
+		templateUrl: 'app/views/listGroups.html',
+		controller: 'groupsCtrl'
+	}).
+	when('/addGroup', {
+		templateUrl: 'app/views/addGroup.html',
+		controller: 'addGroupsCtrl'
+	}).
+	when('/editGroup/:group_id', {
+		templateUrl: 'app/views/editGroup.html',
+		controller: 'editGroupsCtrl'
+	}).
+	otherwise({
+		redirectTo: '/'
+	});
+});
+
+
