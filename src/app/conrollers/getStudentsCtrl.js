1a5a834fcc59ca782b889b01d0f51f5f782180aa
yprodev <yprodanchuk@gmail.com>
2015-11-01 20:40:14 +0200

    Minor changes

--- src/app/conrollers/getStudentsCtrl.js
+++ src/app/conrollers/getStudentsCtrl.js
@@ -7,17 +7,55 @@ angular.module('app')
 			.then(function (response) {
 				$scope.students = response;
 				$scope.noData = "There is no entities here.";
-				$scope.list = $scope.students.list;
-				console.log($scope.list);
 			});
 
 		// Show edit panel for a student
-		$scope.showEditingForm = function (studentId) {
-			$scope.editingStudent = studentId;
+		$scope.showEditingForm = function (stud) {
+			console.log(stud);
+			if (stud !== null) {
+				$scope.currId = stud.user_id;
+			}
+				$scope.editingStudent = stud;
+			// $scope.editingStudent = stud.user_id;
+
 		};
 
-		// Editing student record functionality
-		// HERE WE ARE STOPPED
+		// Editing and updating student record functionality
+		// $scope.editStud = function () {
+
+		// 	// Put student data we need to update
+		// 	var editStudData = {
+		// 		// Students Values
+		// 		gradebook_id: $scope.editingStudent.gradebook_id,
+		// 		student_surname: $scope.editingStudent.student_surname,
+		// 		student_name: $scope.editingStudent.student_name,
+		// 		student_fname: $scope.editingStudent.student_fname,
+		// 		group_id: $scope.editingStudent.group_id,
+		// 		plain_password: $scope.editingStudent.plain_password
+		// 	};
+
+		// 	var editingData = $scope.editingStudent;
+
+		// 	editingData.student_fname = $scope.students.student_fname[editingData.user_id];
+		// 	editingData.student_name = $scope.students.student_name[editingData.user_id];
+		// 	editingData.student_surname = $scope.students.student_surname[editingData.user_id];
+		// 	editingData.gradebook_id = $scope.students.student_gradebook[editingData.user_id];
+		// 	editingData.plain_password = $scope.students.plain_password[editingData.user_id];
+		// 	editingData.group_name = $scope.students.group_name[editingData.group_id];
+
+		// 	var currId = $scope.currId;
+
+		// 	entitiesSrvc.updateEntity($scope.thisEntity, currId, editStudData)
+		// 		.then(function (response) {
+		// 			if(response.data.response == 'ok') {
+		// 				for (var i = 1; i < $scope.students.list.length; i++) {
+		// 					if ($scope.students.list[i].user_id == currId) {
+		// 						$scope.students.list[i] = editingData;
+		// 					}
+		// 				} // END for loop
+		// 			} // END if statement for status 'OK'
+		// 		}); // END .then
+		// };
 
 
 
@@ -49,12 +87,5 @@ angular.module('app')
 				}); //END .then
 			$scope.confirmDelete();
 		}; // END deleteStudent
-
-
-		// Basic approach to get entities from backend
-		// getStudentsSrvc.getStudents().then(function (response) {
-		// 	$scope.students = response;
-		// 	console.log($scope.students);
-		// });
 
 	}]);
