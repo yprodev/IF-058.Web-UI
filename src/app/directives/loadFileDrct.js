d17d79a96fce90219249e8e564424f932a22cfb7
Sergepl1 <sergei_plahuta@mail.ru>
2015-11-10 01:11:15 +0200

    added functionality

--- src/app/directives/loadFileDrct.js
+++ src/app/directives/loadFileDrct.js
@@ -1,28 +1,4 @@
-/*
-app.directive('loadfileDrct', function() {
-  return {
-    template: '<input id="upload"  class="button" type="file" name="file" accept="image/!*" onchange="angular.element(this).scope().loadFile(this.files)"/> ' +
-    '<img id="imageAttachment" ng-src="{{imagecontent}}" height="200px">',
 
-    link: function(scope, el, attrs) {
-      scope.newEntity = {
-        attachment:''
-      }
-      scope.$watch('showingAdd', function (newValue, oldValue) {
-        if (newValue == false) {
-          console.log(scope.newEntity.attachment)
-          angular.element(document.querySelector('#imagecontent'))[0].attributes["src"].value = "";
-        }
-      })
-      scope.$watch(scope.resetEntity(), function (newValue, oldValue) {
-        console.log('asdasdas')
-        if (newValue == false) {
-          console.log(angular.element(document.querySelector('#imageAttachment'))[0].attributes);
-          angular.element(document.querySelector('#imageAttachment'))[0].attributes["src"].value = "";
-        }
-      })
-    }}
-})*/
 
 app.directive('loadfileDrct', function() {
   return {
@@ -67,7 +43,9 @@ app.directive('fileModel', function() {
         scope.files = files;
         var reader = new FileReader();
         reader.onload = function(e) {
-          scope.editedEntity.new_attachment = e.target.result
+          scope.$apply(function () {
+            scope.editedEntity.new_attachment = e.target.result
+          });
         };
         reader.readAsDataURL(files[0]);
       }
@@ -78,20 +56,4 @@ app.directive('fileModel', function() {
       }
     }}
 })
-/*
 
-
-app.directive('quesText', function($compile) {
-  return {
-    link: function(scope, element, attrs){
-      var a = '<p>asdsad</p>'
-      scope.compiledData = $compile(scope.editedEntity.new_question_text)(scope);
-      console.log(scope.compiledData[0].innerText)
-      //console.log($sce.trustAsHtml(scope.editedEntity.new_question_text))
-      //ng-bind-html="entity.question_text | unsafe"
-
-    }
-  }
-})
-
-*/
