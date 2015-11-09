fc0f60353b6a6d620e836b872e37e770701efd14
maneken <volodymyrkulchak@gmail.com>
2015-11-10 01:22:31 +0200

    add timeTable entity

--- src/app/app.js
+++ src/app/app.js
@@ -55,7 +55,15 @@ app.value("entityObj", {
       true_answer: '',
       answer_text: '',
       attachment: ''
-    }
+    },
+    "timeTable": {
+      by: {
+        parentEntity: "subject"
+      },
+      group_name: '',
+      event_date: ''
+      }
+
 
     //... and other entities
   });
@@ -136,6 +144,11 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
 			url:'/students/addStudent',
 			templateUrl: 'app/views/addStudentRecord.html',
 			controller: 'addStudentCtrl'
+		}).
+		state('admin.timeTable', {
+			url:'/timeTable/:id',
+			templateUrl: 'app/views/timeTableList.html',
+			controller: 'entitiesCtrl'
 		}).
 		state('admin.usersTabs.addAdmin', {
 			url: '/addAdmin',
