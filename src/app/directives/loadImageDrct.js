32d9a67c72ecf7fd9e3a7761fa63f10c3069e5bd
yprodev <yprodanchuk@gmail.com>
2015-11-16 17:56:04 +0200

    Uploading image with popover. Everything works perfectly

--- src/app/directives/loadImageDrct.js
+++ src/app/directives/loadImageDrct.js
@@ -1,7 +1,7 @@
 app.directive('imageLoad', ['$timeout', function ($timeout) {
 
 	// imageLoad directive link function
-	function link (scope, element, attrs, ctrls) {
+	function link ($scope, $element, $attrs, $ctrls) {
 
 	}
 
@@ -9,12 +9,17 @@ app.directive('imageLoad', ['$timeout', function ($timeout) {
 	function imageLoadCtrl ($scope) {
 
 		$scope.studPhoto = {
-			src: ''
+			src: '',
+			name: ''
 		};
 
 		var ctrl = this;
 		ctrl.setImgSrc = function (src) {
 			$scope.studPhoto.src = src; //END observe
+		};
+
+		ctrl.setImgName = function (name) {
+			$scope.studPhoto.name = name; //END observe
 		};
 
 	}
@@ -23,7 +28,7 @@ app.directive('imageLoad', ['$timeout', function ($timeout) {
 		restrict: 'E',
 		template: [
 			'<div class="form-group">',
-				'<image-label image="{{ studPhoto.src }}"></image-label>',
+				'<image-label image="{{ studPhoto.src }}" image-name="{{ studPhoto.name }}"></image-label>',
 				'<image-input></image-input>',
 			'</div>'
 			].join('\n'),
@@ -36,70 +41,58 @@ app.directive('imageLoad', ['$timeout', function ($timeout) {
 }]);
 
 
-
-
-
-
-
 app.directive('imageLabel', ['$timeout', function ($timeout) {
+
+	function link ($scope, $element, $attrs, ctrls) {
+		var parentCtrl = ctrls[0]
+			, img;
+
+			$attrs.$observe('image', function () {
+				// Inner variables
+				var picSrc = $scope.image
+					, picName = $scope.imageName;
+
+				if (picSrc) {
+					$($element).popover({
+						html: true,
+						trigger: 'hover',
+						placement: 'top',
+						title: (picName !== '' && picName !== undefined) ? picName : 'there is no name',
+						content: function () {
+							return '<img class="img-popover" src="' + picSrc + '" />';
+						}
+					}); // END jQuery element selecting
+				} // END if statement
+
+			});
+	}// END link function
+
 
 	return {
 		restrict: 'E',
 		scope: {
-			image: '@'
+			image: '@',
+			imageName: '@'
 		},
-		require: ['^imageLoad', 'imageLabel'],
+		require: ['^imageLoad'],
 		template: [
 			'<label class="btn btn-default btn-sm" for="photo">',
 				'<span class="glyphicon glyphicon-cloud-upload"></span>',
 				'<span class="file-name">Choose Student\'s Photo</span>',
 			'</label>'
 		].join('\n'),
-		controller: function ($scope, $element) {
-
-		},
-		link: function ($scope, $element, $attrs, ctrls) {
-
-			var parentCtrl = ctrls[0]
-				, ownCtrl = ctrls[1]
-				, img;
-
-				console.log('SI before changing ', $scope.image);
-
-				// -------------------------------------------------
-				$timeout(function () {
-					console.log('popoover attrs ', $attrs);
-				}, 3000);
-				// -------------------------------------------------
-
-				console.log('hiibusa');
-
-				$attrs.$observe('image', function () {
-					img = $scope.image;
-
-					$($element).popover({
-						html: true,
-						trigger: 'hover',
-						placement: 'top',
-						title: 'SOME INFO',
-						content: function () {
-							return '<img ng-src="' + img + '" />';
-						}
-					});
-
-				});
-
-		}// END link function
+		link: link
 	};
+
 }]);
 
 
 
 
 
-app.directive('imageInput', ['$timeout', function ($timeout) {
+app.directive('imageInput', [function () {
 
-	// Cut file name
+	// Cut file name function
 	function fileCutName (str, slength) {
 		if (str.length >= 10) {
 			return '... ' + str.slice(slength);
@@ -107,47 +100,48 @@ app.directive('imageInput', ['$timeout', function ($timeout) {
 		return str;
 	}
 
-	return {
 
+	function link ($scope, $element, $attrs, ctrls) {
+		var parentCtrl = ctrls[0]
+			, fileTarget
+			, fileName
+			, el = $element.parent().find('.file-name')
+			, reader;
+		// This functionality needs to be here ...
+		// ... because of access to parentCtrl methods after onload event
+		$element.bind('change', function (changeEvent) {
+
+			// Define fileTarget and fileName after change method (jqLite method)
+			fileTarget = changeEvent.target.files[0];
+			fileName = fileTarget.name;
+
+			// Define cutted nama of the file and change the value of the button 
+			$scope.cutName = fileCutName(fileName, -11);
+			el.text($scope.cutName); // HERE must be a problem
+
+			reader = new FileReader();
+			reader.onload = function (loadEvent) {
+				$scope.$apply(function () {
+					$scope.fileread = loadEvent.target.result;
+
+					// Transfer data to the parent directive controller
+					parentCtrl.setImgSrc(reader.result);
+					parentCtrl.setImgName($scope.cutName);
+				});
+			};
+			reader.readAsDataURL(fileTarget);
+		}); // END element bind
+	} // END LINK FUNCTION
+
+
+
+	return {
 		template: '<input type="file" name="studPhoto" id="photo" class="form-control inputfile" aria-describedby="helpPhoto" tabindex="9" image="studPhoto" accept="image/*">',
 		scope: {
 			fileread: '=image',
 		},
 		require: ['^imageLoad'],
-		link: function ($scope, $element, $attrs, ctrls) {
-			var parentCtrl = ctrls[0]
-				, ownCtrl = ctrls[1]
-				, fileTarget
-				, fileName
-				, el = $element.parent().find('.file-name')
-				, reader;
-
-			// This functionality needs to be here ...
-			// ... because of access to parentCtrl methods after onload event
-			$element.bind('change', function (changeEvent) {
-
-				// Define fileTarget and fileName after change method (jqLite method)
-				fileTarget = changeEvent.target.files[0];
-				fileName = fileTarget.name;
-
-				// Define cutted nama of the file and change the value of the button 
-				$scope.cutName = fileCutName(fileName, -11);
-				el.text($scope.cutName); // HERE must be a problem
-
-				reader = new FileReader();
-				reader.onload = function (loadEvent) {
-					$scope.$apply(function () {
-						$scope.fileread = loadEvent.target.result;
-
-						// Transfer data to the parent directive controller
-						parentCtrl.setImgSrc($scope.fileread);
-					});
-				};
-				reader.readAsDataURL(fileTarget);
-
-			}); // END element bind
-
-		} // END LINK FUNCTION
+		link: link
 	};
 }]);
 
