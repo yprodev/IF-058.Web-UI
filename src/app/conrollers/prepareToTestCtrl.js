ea75491c3dce38ff1d5faaff504eaab2a432fb2c
Volodymyr Rohozhynskyi <volodymyr_r@i.ua>
2015-11-25 22:28:14 +0200

    aligned code, deleted consollogs and comments in test player and loginCtrl

--- src/app/conrollers/prepareToTestCtrl.js
+++ src/app/conrollers/prepareToTestCtrl.js
@@ -1,108 +1,96 @@
 var testPlayerApp = angular.module('testPlayerApp', ['ui.router']);
-//список вопросов
-testPlayerApp.controller('prepareToTestCtrl', ['$scope', '$rootScope', 'userSrvc', '$stateParams', '$state', '$q','$timeout', function ($scope, $rootScope, userSrvc, $stateParams, $state, $q, $timeout) {
-    
+//список питань
+testPlayerApp.controller('prepareToTestCtrl', ['$scope', '$rootScope', 'userSrvc', '$stateParams', '$state', '$q', '$timeout',
+    function ($scope, $rootScope, userSrvc, $stateParams, $state, $q, $timeout) {
 
     var testData = {
-    counter: '',
-    startTime: '',
-    questionList: ''
-  }
+        counter: '',
+        startTime: '',
+        questionList: ''
+    };
 
-  $rootScope.testData = testData
-  var data = localStorage.userId/*'4'*///захардкоджено, внести в базу нужне значення і поміняти
-  console.log('userId',data)
-  var url = 'result/getRecordsByStudent/'
-  var id = $stateParams.id
-  function unique(resp) {
-    var repeatedTest_id = {
-      test_id: '',
-      result: ''
-    }
-    for (var i = 0; i < resp.data.length; i++) {
-      if (resp.data[i].test_id == id){
-        repeatedTest_id.test_id = resp.data[i].test_id;
-        repeatedTest_id.result++;
-      }
-    }
-    return repeatedTest_id
-  }
-  $scope.showInformModal = function (infMsg) {
-    $scope.infMsg = infMsg;
-    angular.element(document.querySelector('#informModal')).modal();
-  };
-  $scope.getRecordsByStudent = function () {
-    userSrvc.getInfoForStudent(url, data).then(function (resp) {
-      var data = unique(resp).test_id
-      var result = unique(resp).result
-      var url = 'test/getRecords/'
-      return userSrvc.getInfoForStudent(url, data, result)
-    }).then(function(test){
-      $scope.timeForTest = test[0].data[0].time_for_test;
-      console.log($scope.timeForTest, 'timeForTest');
-      if (test[0].data[0].attempts < test[1]){
-        alert('Немає предметів з доступними тестами для вашої групи')//убрати коли запрацює модалка
-     $scope.showInformModal("Немає предметів з доступними тестами для вашої групи");
-      }
+    $rootScope.testData = testData;
+    var data = localStorage.userId;
+    var url = 'result/getRecordsByStudent/';
+    var id = $stateParams.id;
 
-      var data =  $stateParams.id
-      var url = 'TestDetail/getTestDetailsByTest/'
-      return userSrvc.getInfoForStudent(url, data)
-    }).then(function(resp){
-      //console.log('testDetails',resp)
-      console.log('id',resp.data[0].id)
-      if (!resp.data[0].id){
-        alert('Немає деталей тесту')
-         $scope.showInformModal("Немає деталей тесту");//дописати щоб запрацювала модалка
-      }
-     /* if (){
+    function unique(resp) {
+        var repeatedTest_id = {
+            test_id: '',
+            result: ''
+        };
+        for (var i = 0; i < resp.data.length; i++) {
+            if (resp.data[i].test_id == id) {
+                repeatedTest_id.test_id = resp.data[i].test_id;
+                repeatedTest_id.result++;
+            }
+        }
+        return repeatedTest_id;
+    };
 
-      } */
-      var id = $stateParams.id
-      localStorage.testId = id;
-      var data = [id, resp.data[0].level, resp.data[0].tasks]
-      var url = 'question/getQuestionIdsByLevelRand/'
-      return userSrvc.getInfoForStudent(url, data)
-    }).then(function(resp){
-      var questionList = []
-      for (i in resp.data){
-        questionList.push(resp.data[i].question_id)
-      }
-      //localStorage.questionList = questionList;
-      testData.questionList = questionList;
-      $scope.questionsQuantity = questionList.length;
-        console.log(questionList, "&&&&&&&&&&&&&&&&");
-    }, function(error){
-      console.log('Відсутні деталі тесту по данному тесту')
-      alert('Відсутні деталі тесту по данному тесту')
-    })
-    
-  }
+    $scope.showInformModal = function (infMsg) {
+        $scope.infMsg = infMsg;
+        angular.element(document.querySelector('#informModal')).modal();
+    };
+    $scope.getRecordsByStudent = function () {
+        userSrvc.getInfoForStudent(url, data).then(function (resp) {
+            var data = unique(resp).test_id;
+            var result = unique(resp).result;
+            var url = 'test/getRecords/';
+            return userSrvc.getInfoForStudent(url, data, result)
+        }).then(function (test) {
+            $scope.timeForTest = test[0].data[0].time_for_test;
+            if (test[0].data[0].attempts < test[1]) {
+                alert('Немає предметів з доступними тестами для вашої групи');
+                $scope.showInformModal("Немає предметів з доступними тестами для вашої групи");
+            };
+
+            var data = $stateParams.id;
+            var url = 'TestDetail/getTestDetailsByTest/';
+            return userSrvc.getInfoForStudent(url, data)
+        }).then(function (resp) {
+            if (!resp.data[0].id) {
+                alert('Немає деталей тесту')
+                $scope.showInformModal("Немає деталей тесту");//дописати щоб запрацювала модалка
+            };
+
+            var id = $stateParams.id;
+            localStorage.testId = id;
+            var data = [id, resp.data[0].level, resp.data[0].tasks];
+            var url = 'question/getQuestionIdsByLevelRand/';
+            return userSrvc.getInfoForStudent(url, data)
+        }).then(function (resp) {
+            var questionList = [];
+            for (var i in resp.data) {
+                questionList.push(resp.data[i].question_id);
+            };
+            testData.questionList = questionList;
+            $scope.questionsQuantity = questionList.length;
+        }, function (error) {
+            alert('Відсутні деталі тесту по данному тесту');
+        })
+    };
 
     var data = '';
     var url = 'TestPlayer/getTimeStamp';
     userSrvc.getInfoForStudent(url, data).then(function (resp) {
-      testData.startTime = new Date(resp.data.unix_timestamp * 1000);
+        testData.startTime = new Date(resp.data.unix_timestamp * 1000);
     });
-  console.log('testData', testData)
-  $scope.getRecordsByStudent()
-  $scope.startTest = function(){
-    console.log('hello')
-    function timer(testDuration) {
-    testData.counter = $scope.timeForTest * 60;
-    //console.log($scope.counter, "77777777777777777777");
-    $scope.onTimeout = function(){
-        testData.counter--;
-        mytimeout = $timeout($scope.onTimeout,1000);
-    }
-    var mytimeout = $timeout($scope.onTimeout,1000);
-      $scope.stop = function() {
-      $timeout.cancel(mytimeout);
-    }
-}    /*if ()*/
-timer();
-  }
-
+    $scope.getRecordsByStudent()
+    $scope.startTest = function () {
+        function timer(testDuration) {
+            testData.counter = $scope.timeForTest * 60;
+            $scope.onTimeout = function () {
+                testData.counter--;
+                mytimeout = $timeout($scope.onTimeout, 1000);
+            }
+            var mytimeout = $timeout($scope.onTimeout, 1000);
+            $scope.stop = function () {
+                $timeout.cancel(mytimeout);
+            }
+        }
+        timer();
+    };
 }]);
 
 
--- src/app/conrollers/testPlayerCtrl.js
+++ src/app/conrollers/testPlayerCtrl.js
@@ -1,74 +1,54 @@
-testPlayerApp.controller('userQuestionListCtrl', ['$scope', '$rootScope', 'userSrvc', '$stateParams', '$state', '$q','$timeout', function ($scope, $rootScope, userSrvc, $stateParams, $state, $q, $timeout) {
-  $scope.beginTest = function(){
-    console.log('testData', $rootScope.testData)
-/*    if (!$scope.showTest){
-    $scope.showTest = true;
-    /!*angular.element(element.getElementsById(".multi-files"))*!/
-    }*/
-    var userId = localStorage.userId;
-    var testId = localStorage.testId;
+testPlayerApp.controller('userQuestionListCtrl', ['$scope', '$rootScope', 'userSrvc', '$stateParams', '$state', '$q', '$timeout',
+    function ($scope, $rootScope, userSrvc, $stateParams, $state, $q, $timeout) {
+        $scope.beginTest = function () {
+            var userId = localStorage.userId;
+            var testId = localStorage.testId;
 
+            var questionArray = $rootScope.testData.questionList;
+            var question;
+            if ($stateParams.id) {
+                question = +questionArray[$stateParams.id];
+            } else {
+                question = questionArray[0];
+                $scope.selected = 0;
+            };
 
-    /*userSrvc.getTestInfo(userId, testId).then(function (resp) {
-      $state.go('user.testPlayer');
-    });*/
-     /* var id = localStorage.userId + '/' + localStorage.testId
-    var url = 'log/startTest/'
-    userSrvc.getInfoForStudent(url, id).then(function (resp) {
-      console.log(resp)
-    })*/
+            $scope.chosenQuestion = function (questNumber, index) {
+                $scope.selected = index;
+                $scope.questNumber = questNumber;
+                nextQuestion(questNumber);
+            };
 
-      var questionArray = $rootScope.testData.questionList
-      var question;
-      if ($stateParams.id) {
-          question = +questionArray[$stateParams.id] ;
-      } else {
-          question = questionArray[0];
-          $scope.selected = 0;
-      }
+            $scope.firstQuestion = function (question) {
+                nextQuestion(question);
+            }
 
-      //$scope.nextQuestion(questNumber)
-      $scope.chosenQuestion = function(questNumber, index){
-          $scope.selected = index;
-          console.log($scope.selected, "%%%%%%%%%%%%%%%%%%%%%%");
-          $scope.questNumber = questNumber;
-          nextQuestion(questNumber);
-      };
-//$scope.chosenQuestion(question);
+            $scope.firstQuestion(question);
 
-      $scope.firstQuestion = function(question) {
-          nextQuestion(question);
-      }
-      console.log("huy");
-      $scope.firstQuestion(question);
+            function nextQuestion(data) {
+                var questionUrl = 'question/getRecords/';
+                var answerUrl = 'SAnswer/getAnswersByQuestion/';
+                $q.all([
+                    userSrvc.getInfoForStudent(questionUrl, data),
+                    userSrvc.getInfoForStudent(answerUrl, data)
+                ])
+                    .then(function (resp) {
+                        $scope.question = resp[0].data[0].question_text;
+                        $scope.answers = resp[1].data;
+                        $scope.type = resp[0].data[0].type == '1' ? 'radio' : 'checkbox';
+                    })
+            };
 
-function nextQuestion (data){
-      var questionUrl = 'question/getRecords/';
-      var answerUrl = 'SAnswer/getAnswersByQuestion/';
-      //$scope.questList =
-$q.all([
-    userSrvc.getInfoForStudent(questionUrl, data),
-    userSrvc.getInfoForStudent(answerUrl, data)
-  ])
-   .then(function (resp) {
-      $scope.question = resp[0].data[0].question_text;
-      $scope.answers = resp[1].data;
-      $scope.type = resp[0].data[0].type == '1' ? 'radio' : 'checkbox';
-   })
-}
-   $scope.submitQuestion = function(radioValue) {
-       var answerObj = {}
-       answerObj.selectedAnswers = radioValue;
-       answerObj.selectedQuestion = $scope.questNumber;
-       console.log(answerObj,"AO");
-       answerArray.push(answerObj);
-       console.log(answerArray,"AA");
-   };
-
-  }
-  //наступний запит використовуємо щоб залогініти юзера
-    $scope.beginTest();
-}]);
+            $scope.submitQuestion = function (radioValue) {
+                var answerObj = {};
+                var answerArray = [];
+                answerObj.selectedAnswers = radioValue;
+                answerObj.selectedQuestion = $scope.questNumber;
+                answerArray.push(answerObj);
+            };
+        };
+        $scope.beginTest();
+    }]);
 
 
 
