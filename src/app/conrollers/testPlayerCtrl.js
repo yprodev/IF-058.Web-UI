9eb832d6d4ed9cd286b87f13f34065188f5066aa
Sergepl1 <sergei_plahuta@mail.ru>
2015-12-02 11:53:44 +0200

    added filter to timer

--- src/app/conrollers/testPlayerCtrl.js
+++ src/app/conrollers/testPlayerCtrl.js
@@ -22,34 +22,11 @@ testPlayerApp.controller('userQuestionListCtrl', ['$scope', '$rootScope', 'userS
                           
                           
                           $scope.onTimeout = function(){
-                           /* console.log('timer')*/
                             userSrvc.getInfoForStudent(url, data).then(function (resp) {
-                            /*console.log(resp.data.unix_timestamp, 'time1')
-                            console.log(savedTestData.startTime, 'savedTestData.startTime')*/
-                            //console.log('savedTestData.timeForTest', savedTestData.timeForTest)
-                            console.log(resp.data.unix_timestamp - savedTestData.timeForTest*60*1000, 'time2')
-                            var remainedTime = savedTestData.startTime - (resp.data.unix_timestamp - savedTestData.timeForTest*60)
-                            console.log('remainedTime', remainedTime)
-                            //$scope.counter = new Date(remainedTime * 1000);
-                            console.log('$scope.counter', remainedTime)
-
-                            var date1 = new Date(resp.data.unix_timestamp*1000)
-                            var date2 = new Date((resp.data.unix_timestamp - savedTestData.timeForTest*60) * 1000);
-                            var date3 = new Date(savedTestData.startTime*1000)
-                            var rem = (date3 - date2)
-                            $scope.counter = (rem)
-                            console.log('rem', Math.floor(rem/6000))
-
-                            /*
-
-                            console.log('timeForTest, milliseconds', savedTestData.timeForTest*60*1000)
-                            console.log('date1', date1)
-                            console.log('date2', date2)
-                            console.log('date3', date3)*/
-                            /*console.log(savedTestData.timeForTest, 'timeForTest')
-                            console.log($scope.counter, '$scope.counter')
-                            console.log($scope.counter-savedTestData.timeForTest, '$scope.counter2')*/
-
+                            var timeDifference = new Date((resp.data.unix_timestamp - savedTestData.timeForTest*60) * 1000);
+                            var timeStart = new Date(savedTestData.startTime*1000)
+                            var remeinedTime = (timeStart - timeDifference)
+                            $scope.counter = (remeinedTime/1000)
                             mytimeout = $timeout($scope.onTimeout, 1000);
                           })
                           }
@@ -57,23 +34,6 @@ testPlayerApp.controller('userQuestionListCtrl', ['$scope', '$rootScope', 'userS
                           
                     }
                     timer()
-
-
-                    /*function timer(testDuration) {
-                      testData.counter = $scope.timeForTest * 60;
-                      var mytimeout = $timeout($scope.onTimeout, 1000);
-                      $scope.onTimeout = function () {
-                        testData.counter--;
-                        $rootScope.counter = testData.counter
-                        mytimeout = $timeout($scope.onTimeout, 1000);
-                      }
-                      
-                      $scope.stop = function () {
-                        $timeout.cancel(mytimeout);
-                      }
-                    }*/
-
-
                     var userId = localStorage.userId;
                     var testId = localStorage.testId;
                     var answerObj = {};
--- src/app/filters/entetiesFilter.js
+++ src/app/filters/entetiesFilter.js
@@ -44,14 +44,11 @@ app.filter('correctAnswer', [function() {
   }
 }]);
 app.filter('formatTimer', [function () {
- /* return function (input) {
-    function z(n) {
-      return (n < 10 ? '0' : '') + n;
-    }
-    var seconds = input % 1000;
-    var minutes = Math.floor(input % 1000 / 60);
-    var hours = Math.floor(input / 10000/3600);
+  return function (input) {
+    function z(n) { return (n < 10 ? '0' : '') + n; }
+    var seconds = input % 60;
+    var minutes = Math.floor(input % 3600 / 60);
+    var hours = Math.floor(input / 3600);
     return (z(hours) + ':' + z(minutes) + ':' + z(seconds));
-  };*/
-  
+};
 }]);
